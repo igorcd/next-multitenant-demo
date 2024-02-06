447d7ce1387cbf16a8c426c7a18fa30dd7c2152c
Igor Dantas <igor.dantas64@gmail.com>
2024-02-06 10:19:49 +0000

    -

--- src/slides/slide-5/Slide5.tsx
+++ src/slides/slide-5/Slide5.tsx
@@ -2,7 +2,7 @@ import Slide from "@/components/slide/Slide";
 
 export default function Slide5() {
     return (
-        <Slide id={4}>
+        <Slide id={5}>
             <div className='h-full flex flex-col items-center justify-center p-6 gap-10'>
                 <h1 className='text-5xl font-medium bg-clip-text text-gray-200 text-center max-w-2xl leading-[4rem]'>
                     Subdomain approach
